5ad1b5bd3f2d6dd50133c584361db78a4196213e
DANG NGUYEN <noplstks@gmail.com>
2024-05-11 12:43:40 +0700

    Add cors server

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,14 @@ import exploreRoutes from "./routes/explore.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
+import cors from "cors";
 
 const PORT = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
 dotenv.config();
-
+app.use(cors());
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
